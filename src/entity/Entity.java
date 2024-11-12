f041ab9c03d08d79f5a1a1c49eae697dbf99fb40
ttkien2125 <imgoodatgamesm8@gmail.com>
2024-11-12 23:05:10 +0700

    Chinh sua file Player

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -2,12 +2,18 @@ package src.entity;
 
 import java.awt.image.BufferedImage;
 
+enum Action {
+    Up, Down, Left, Right,
+    Punch, Kick,
+    Idle,
+}
+
 public class Entity {
     public int x,y;
     public int speed;
 
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2, idle, idle2, punch1, punch2, punch3, punch4, kick1, kick2, kick3, kick4;
-    public String direction;
+    public Action action;
 
     public int spriteCounter = 0;
     public int spriteNum = 1;
